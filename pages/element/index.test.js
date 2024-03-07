bdbd0f586f59c2393747b873f568f11dcc4fa3e1
zhenyuWang <13641039885@163.com>
2024-03-07 13:54:52 +0800

    wip: element test case

---
+++ pages/element/index.test.js
@@ -0,0 +1,30 @@
+describe('Element API', () => {
+  let page;
+  const pageUrl = '/pages/element/index';
+
+  beforeAll(async () => {
+    page = await program.reLaunch(pageUrl)
+  })
+
+  it('tagName', async () => {
+    const viewEl = await page.$('view')
+    expect(viewEl.tagName).toBe('view')
+    
+    const textEl = await page.$('text')
+    expect(textEl.tagName).toBe('text')
+  })
+  
+  it('$', async () => {
+    const textContainer = await page.$('.text-container')
+    const textEl = await textContainer.$('.my-text')
+    expect(await textEl.text()).toBe('this is text 1')
+  })
+  
+  it('$$', async () => {
+    const textContainer = await page.$('.text-container')
+    const textElList = await textContainer.$$('.my-text')
+    expect(textElList.length).toBe(2)
+    expect(await textElList[0].text()).toBe('this is text 1')
+    expect(await textElList[1].text()).toBe('this is text 2')
+  })
+});
