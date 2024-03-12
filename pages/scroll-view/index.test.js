2ac065f3f61553d888ca13a7ee607689b5c68d39
wangjinxin613 <1749574663@qq.com>
2024-03-12 13:32:38 +0800

    feat(scroll-view): 新增 scroll-view 组件测视例

---
+++ pages/scroll-view/index.test.js
@@ -0,0 +1,90 @@
+
+describe('scroll-view', () => {
+
+	let page;
+	beforeAll(async () => {
+		page = await program.reLaunch('/pages/scroll-view/index');
+		await page.waitFor(300);
+	});
+
+	//检测竖向可滚动区域
+	it('check_scroll_height', async () => {
+		await page.setData({
+			scrollX: false
+		})
+		await page.waitFor(300);
+		const value = await page.callMethod('checkScrollHeight')
+		expect(value).toBe(true) 
+	})
+
+	//检测竖向scrolltop属性赋值
+	it('check_scroll_top', async () => {
+	    await page.setData({
+	        scrollTop: 600
+	    })
+	    await page.waitFor(600)
+	    const element = await page.$('#scrollViewY')
+	    const scrollTop = await element.property("scrollTop")
+	    console.log("check_scroll_top---"+scrollTop)
+	    expect(scrollTop-600).toBeGreaterThanOrEqual(0)
+	})
+
+	//检测竖向scroll_into_view属性赋值
+	it('check_scroll_into_view_top', async () => {
+	    await page.setData({
+	        scrollIntoView: "item3"
+	    })
+	    await page.waitFor(600)
+	    const element = await page.$('#scrollViewY')
+	    const scrollTop = await element.property("scrollTop")
+	    console.log("check_scroll_into_view_top--"+scrollTop)
+	    await page.setData({
+	        scrollIntoView: ""
+	    })
+	    expect(scrollTop-570).toBeGreaterThanOrEqual(0)
+	})
+
+	//检测横向可滚动区域
+	it('check_scroll_width', async () => {
+	  await page.setData({
+	      scrollX: true
+	  })
+	  await page.waitFor(300);
+	  const value = await page.callMethod('checkScrollWidth')
+	  expect(value).toBe(true)
+	})
+
+	//检测横向scrollLeft属性赋值
+	it('check_scroll_left', async () => {
+	  await page.setData({
+	      scrollLeft: 600
+	  })
+	  await page.waitFor(600)
+	  const element = await page.$('#scrollViewX')
+	  const scrollLeft = await element.property("scrollLeft")
+	  console.log("check_scroll_left---"+scrollLeft)
+	  expect(scrollLeft-600).toBeGreaterThanOrEqual(0)
+	})
+	
+	// 检测srollTo
+	it('scrollTo', async () => {
+		const element = await page.$('#scrollViewX')
+		await element.scrollTo(82, 0)
+		expect(await element.property("scrollLeft")).toBe(82)
+	})
+
+	//检测横向scroll_into_view属性赋值
+	it('check_scroll_into_view_left', async () => {
+	  await page.setData({
+	      scrollIntoView: "horizontal_item3"
+	  })
+	  await page.waitFor(600)
+	  const element = await page.$('#scrollViewX')
+	  const scrollLeft = await element.property("scrollLeft")
+	  console.log("check_scroll_into_view_left--"+scrollLeft)
+	  await page.setData({
+	      scrollIntoView: ""
+	  })
+	  expect(scrollLeft-930).toBeGreaterThanOrEqual(0)
+	})
+});
