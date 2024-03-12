33818a80e486fa5f5d5cc955de9cdd1ef44f0c95
wangjinxin613 <1749574663@qq.com>
2024-03-12 18:37:36 +0800

    feat: add element.swiperTo

---
+++ pages/swiper/index.test.js
@@ -0,0 +1,17 @@
+let page;
+const pageUrl = '/pages/swiper/index';
+describe(pageUrl, () => {
+
+	beforeAll(async () => {
+		page = await program.reLaunch(pageUrl)
+	})
+
+	it('swipeTo', async () => {
+		const element = await page.$('#swiper')
+		await element.swipeTo(1);
+		expect(Number(await element.attribute('current'))).toEqual(1);
+		await element.swipeTo(2);
+		expect(Number(await element.attribute('current'))).toEqual(2);
+	})
+
+})
