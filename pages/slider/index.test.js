8cb439e1a023a1b8fe0fa4bc8d5303e97788901f
wangjinxin613 <1749574663@qq.com>
2024-03-12 18:57:02 +0800

    feat: add element.slideTo

---
+++ pages/slider/index.test.js
@@ -0,0 +1,18 @@
+let page;
+const pageUrl = '/pages/slider/index';
+describe(pageUrl, () => {
+
+	beforeAll(async () => {
+		page = await program.reLaunch(pageUrl)
+	})
+
+	it('slideTo', async () => {
+		const element = await page.$('#slider')
+		expect(Number(await element.attribute('value'))).toEqual(20);
+		await element.slideTo(30);
+		expect(Number(await element.attribute('value'))).toEqual(30);
+		await element.slideTo(90);
+		expect(Number(await element.attribute('value'))).toEqual(90);
+	})
+
+})
