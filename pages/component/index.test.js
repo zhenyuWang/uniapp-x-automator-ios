0a71cce462a530cbf70fac17007eea4b98510f7f
wangjinxin613 <1749574663@qq.com>
2024-03-26 15:23:02 +0800

    feat: 完善测试用例

--- pages/component/index.test.js
+++ pages/component/index.test.js
@@ -29,23 +29,38 @@ describe(pageUrl, () => {
 		})
 		expect(await element.data('text')).toBe('hello world 2024')
 	})
-	
+
 	it('callMethod', async () => {
 		const element = await page.$('.sub-component')
 		await element.callMethod('addNum')
 		expect(JSON.stringify(await element.data('nums'))).toBe('[1,2,3,4,5]')
 	})
-	
+
 	it('callMethod with params', async () => {
 		const element = await page.$('.sub-component')
 		await element.callMethod('addNum', 8)
 		expect(JSON.stringify(await element.data('nums'))).toBe('[1,2,3,4,5,8]')
 	})
-	
+
 	it('callMethod again', async () => {
 		const element = await page.$('.sub-component')
 		await element.callMethod('deleteNum')
 		expect(JSON.stringify(await element.data('nums'))).toBe('[1,2,3,4,5]')
 	})
-	
+
+	// 多个子组件测试方法调用
+	it('callMethod other component', async () => {
+		const view = await page.$('#view')
+		expect(view.nodeId).toBeNull()
+		const subview = await page.$('#subview')
+		expect(subview.nodeId).toBeNull()
+		const pickerview = await page.$('#picker')
+		expect(pickerview.nodeId).toBeNull()
+		const header = await page.$('#header')
+		expect(header.nodeId).toBeGreaterThanOrEqual(1)
+		const title = await header.callMethod('changeTitle', 'header edited')
+		expect(title).toBe('header edited')
+		expect(await header.data('title')).toBe('header edited')
+	})
+
 })
---
+++ pages/video/index.test.js
@@ -0,0 +1,15 @@
+jest.setTimeout(10000000);
+let page;
+const pageUrl = '/pages/video/index';
+describe(pageUrl, () => {
+
+	beforeAll(async () => {
+		page = await program.reLaunch(pageUrl)
+		await page.waitFor(1000);
+	})
+
+	it('callContextMethod play', async () => {
+		const element = await page.$('#video')
+		console.log(await element.callContextMethod('play'))
+	})
+})
